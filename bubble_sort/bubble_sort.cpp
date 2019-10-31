fa78679fe39a95e3526979ab8b286710a7a454d1
Peter <piotrszach@icloud.com>
2019-10-31 14:48:01 +0100

    The bubble sorting algorithm has been implemented

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+void bubble_sort(vector<int> *array)
+{
+    for(int i=0; i<(*array).size(); ++i)
+    {
+        for(int j=0; j<(*array).size()-1-i; ++j)
+        {
+            if((*array)[j]>(*array)[j+1]) swap((*array)[j],(*array)[j+1]);
+        }
+    }
+}
 int main()
 {
-	vector<int> array;
 
 }
